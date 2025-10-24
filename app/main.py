1a6b7cad0ec2aeacd1b3a8693694aff391bb1ec7
dkkinyua <denzelkinyua11@gmail.com>
2025-10-24 11:58:25 +0300

    Commit: Add Redis init_redis method to initialize Redis Add middleware for verifying and validating API keys from the browser headers

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 from app.core.config import settings
-from contextlib import asynccontextmanager
 from app.database.db import MongoDB
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from app.middleware.auth import RapidAPIKeyMiddleware, init_redis, redis_client
 from app.routers import electricity, economic, energy
+from app.utils.alert import logger
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # start the database
     await MongoDB.connect()
+    await init_redis()
 
     yield # the database stays on as long as the app is alive
 
     await MongoDB.close()
-    
+    if redis_client:
+        await redis_client.close()
+        logger("Redis connection closed successfully!")
+
 
 app = FastAPI(
     lifespan = lifespan,
@@ -25,3 +31,13 @@ app = FastAPI(
 app.include_router(electricity.router, prefix="/api/v1/electricity", tags=["Electricity"])
 app.include_router(economic.router, prefix="/api/v1/economic", tags=["Economic"])
 app.include_router(energy.router, prefix="/api/v1/energy", tags=["Energy"])
+
+# add middleware and test route
+app.add_middleware(RapidAPIKeyMiddleware)
+
+@app.get("/api/v1/test")
+def validate_api():
+    try:
+        return {"Message": "API Key OK"}
+    except Exception as e:
+        return {"Error": "Invalid or missing API Keys"}
